884fbd93038acf2b6c967a768b4aaba696fce9c0
vinnp <nguyenvinh8744@gmail.com>
2024-10-22 15:15:47 +0700

    update_home_page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,8 @@ import AosAnimate from "@/components/aos-animate";
 import FooterMenu from "@/components/footer-menu";
 import Search from "@/components/search";
 import type { Metadata } from "next";
+import Image from "next/image";
+import SearchMobile from "@/components/search-mobile";
 
 export const metadata: Metadata = {
   title: "Trang chủ",
@@ -26,6 +28,52 @@ export default function Home() {
   return (
     <Fragment>
       <Search />
+      {/* Search Mobile */}
+      <div className="mt-[68px] block lg:hidden">
+        <div className="mt-4 h-[828px]">
+          <div className="absolute inset-0 h-[828px]">
+            <Image
+              priority
+              src="/bg-image.png"
+              width={500}
+              height={584}
+              className="object-cover w-full h-full"
+              alt="Background"
+            />
+          </div>
+          <div
+            className="absolute w-full h-[828px]"
+            style={{
+              backgroundImage:
+                "linear-gradient(180deg, #04349A 0%, rgba(23, 85, 220, 0.5) 100%)",
+            }}
+          ></div>
+          <div className="relative z-[1]">
+            <h3 className="pt-8 text-xl lg:text-2xl font-bold text-center text-white">
+              Bắt đầu hành trình với HappyBook
+            </h3>
+            {/* Search Bar */}
+            <div className="flex items-center px-3 my-4">
+              <input
+                type="text"
+                placeholder="Tìm theo điểm đến, hoạt động"
+                className="p-2 w-full rounded-l-lg text-gray-700 h-12"
+              />
+              <button className="bg-blue-500 px-3 rounded-r-lg w-12 h-12">
+                <Image
+                  src="/icon/search.svg"
+                  alt="Search icon"
+                  className="h-10"
+                  width={20}
+                  height={20}
+                  style={{ width: 20, height: 20 }}
+                />
+              </button>
+            </div>
+            <SearchMobile />
+          </div>
+        </div>
+      </div>
       <main className="w-full bg-white relative z-2 rounded-2xl top-[-12px]">
         <div className="pt-7 px-3 lg:px-[50px] xl:px-[80px] max__screen">
           <Banner></Banner>
--- src/components/header-mobile.tsx
+++ src/components/header-mobile.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import HeaderMobileMenu from "./header-mobile-menu";
 import { Fragment } from "react";
-import SearchMobile from "./search-mobile";
 
 export default function HeaderMobile() {
   return (
@@ -23,52 +22,6 @@ export default function HeaderMobile() {
           </div>
           {/* Menu */}
           <HeaderMobileMenu />
-        </div>
-      </div>
-      {/* Search */}
-      <div className="mt-[68px] block lg:hidden">
-        <div className="mt-4 h-[828px]">
-          <div className="absolute inset-0 h-[828px]">
-            <Image
-              priority
-              src="/bg-image.png"
-              width={500}
-              height={584}
-              className="object-cover w-full h-full"
-              alt="Background"
-            />
-          </div>
-          <div
-            className="absolute w-full h-[828px]"
-            style={{
-              backgroundImage:
-                "linear-gradient(180deg, #04349A 0%, rgba(23, 85, 220, 0.5) 100%)",
-            }}
-          ></div>
-          <div className="relative z-[1]">
-            <h3 className="pt-8 text-xl lg:text-2xl font-bold text-center text-white">
-              Bắt đầu hành trình với HappyBook
-            </h3>
-            {/* Search Bar */}
-            <div className="flex items-center px-3 my-4">
-              <input
-                type="text"
-                placeholder="Tìm theo điểm đến, hoạt động"
-                className="p-2 w-full rounded-l-lg text-gray-700 h-12"
-              />
-              <button className="bg-blue-500 px-3 rounded-r-lg w-12 h-12">
-                <Image
-                  src="/icon/search.svg"
-                  alt="Search icon"
-                  className="h-10"
-                  width={20}
-                  height={20}
-                  style={{ width: 20, height: 20 }}
-                />
-              </button>
-            </div>
-            <SearchMobile />
-          </div>
         </div>
       </div>
     </Fragment>
