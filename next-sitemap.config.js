1bc814b77563201d3ac56ff304c8cfcb72122e41
xuanhanh.dev <xuanhanh.setdy@gmail.com>
2024-12-25 02:28:09 +0700

    config generate sitemap with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,39 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_UR || "http://client.happybooktravel.com",
+  changefreq: "daily",
+  priority: 1,
+  generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: process.env.SITE_URL || "http://client.happybooktravel.com",
+      hreflang: "vi",
+    },
+  ],
+  additionalPaths: async (config) => [await config.transform(config, "/")],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+  exclude: ["/server-sitemap-index.xml"], // <= exclude here
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    additionalSitemaps: [
+      `${
+        process.env.SITE_URL || "http://client.happybooktravel.com"
+      }/server-sitemap-index.xml`,
+    ],
+  },
+};
---
+++ src/app/server-sitemap-index.xml/route.ts
@@ -0,0 +1,18 @@
+// app/server-sitemap-index.xml/route.ts
+import { getServerSideSitemapIndex } from "next-sitemap";
+
+export async function GET(request: Request) {
+  const api = process.env.API_URL || "http://api.happybooktravel.com";
+  const sitemap = [
+    `${api}/sitemap_news.xml`,
+    `${api}/sitemap_news_categories.xml`,
+    `${api}/sitemap_news_sub_categories.xml`,
+  ];
+
+  for (const type of ["visa", "tour", "hotel"]) {
+    sitemap.push(`${api}/sitemap_${type}.xml`);
+    sitemap.push(`${api}/sitemap_${type}_categories.xml`);
+  }
+
+  return getServerSideSitemapIndex(sitemap);
+}
