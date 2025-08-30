cc72866efe0ed695555b3eb1848c774dbb8a3fdd
xuanhanh.dev <xuanhanh.setdy@gmail.com>
2025-08-30 10:12:13 +0700

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -32,9 +32,8 @@ module.exports = {
     ],
     additionalSitemaps: [
       `${
-        process.env.NEXT_PUBLIC_API_ENDPOINT ||
-        "https://api.happybooktravel.com"
-      }/sitemap.xml`,
+        process.env.NEXT_PUBLIC_SITE_URL || "https://happybooktravel.com"
+      }/server-sitemap-index.xml`,
     ],
   },
 };
--- src/app/server-sitemap-index.xml/route.ts
+++ src/app/server-sitemap-index.xml/route.ts
@@ -2,16 +2,25 @@
 import { getServerSideSitemapIndex } from "next-sitemap";
 
 export async function GET(request: Request) {
-  const api = process.env.NEXT_PUBLIC_API_ENDPOINT || "http://api.happybooktravel.com";
-  const sitemap = [
-    `${api}/sitemap_news.xml`,
-    `${api}/sitemap_news_categories.xml`,
-    `${api}/sitemap_news_sub_categories.xml`,
-  ];
+  const api =
+    process.env.NEXT_PUBLIC_API_ENDPOINT || "http://api.happybooktravel.com";
+  const sitemap = [];
 
-  for (const type of ["visa", "tour", "hotel"]) {
-    sitemap.push(`${api}/sitemap_${type}.xml`);
-    sitemap.push(`${api}/sitemap_${type}_categories.xml`);
+  for (const type of [
+    "pages",
+    "news",
+    "news_categories",
+    "news_sub_categories",
+    "visa",
+    "tour",
+    "hotel",
+    "flight",
+    "combo",
+    "ticket",
+    "yacht",
+    "fast-track",
+  ]) {
+    sitemap.push(`${api}/sitemaps/sitemap_${type}.xml`);
   }
 
   return getServerSideSitemapIndex(sitemap);
