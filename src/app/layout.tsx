6b2f62408d9de70eb5180fb888871a4e68367eba
Tran Hoang Nhu <tran.hoang.nhu.1997@gmail.com>
2026-02-03 12:03:38 +0700

    Add Meta Pixel tracking code (ID: 859144157116787)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,10 @@ export default async function RootLayout({
         {/* End Google Tag Manager */}
 
         {/* Google Ads (gtag.js) */}
-        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-17408673405" strategy="afterInteractive" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17408673405"
+          strategy="afterInteractive"
+        />
         <Script id="google-ads-script" strategy="afterInteractive">
           {`
           window.dataLayer = window.dataLayer || [];
@@ -127,6 +130,32 @@ export default async function RootLayout({
             })(window, document, "clarity", "script", "u1oyy7ej0g");
           `}
         </Script>
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '859144157116787');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=859144157116787&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className={OpenSans.className}>
         <GTMNoScript />
