3a11d64faa1424a73a9c45908226c55bd27876b6
Nguyen Phu Thinh <nguyenphuthinh040507@gmail.com>
2026-04-14 22:49:46 +0700

    [26134][CLIENT] SITEMAP PHÒNG CHỜ THƯƠNG GIA

--- src/app/server-sitemap-index.xml/route.ts
+++ src/app/server-sitemap-index.xml/route.ts
@@ -19,6 +19,7 @@ export async function GET(request: Request) {
     "ticket",
     "yacht",
     "fast-track",
+    "business-lounge",
   ]) {
     sitemap.push(`${api}/sitemaps/sitemap_${type}.xml`);
   }
