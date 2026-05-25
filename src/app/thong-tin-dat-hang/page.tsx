485074dba0f30f827863f16097eab2d0d08ee5e4
HappyBook Travel <happybook.social@gmail.com>
2026-05-26 00:29:24 +0700

    [190526-100][CLIENT] Nút thanh toán trong email thanh toán của khách hàng phải trỏ tới đúng form checkout của module mà khách đặt

--- src/app/thong-tin-dat-hang/page.tsx
+++ src/app/thong-tin-dat-hang/page.tsx
@@ -3,6 +3,7 @@ import { formatMetadata } from "@/lib/formatters";
 import BookingDetail from "./components/BookingDetail";
 import { settingApi } from "@/api/Setting";
 import { siteUrl } from "@/constants";
+import { Suspense } from "react";
 
 export async function generateMetadata({ params }: any): Promise<Metadata> {
   const seo = await settingApi.getCachedMetaSeo();
@@ -30,7 +31,14 @@ export default async function BookingFlight() {
   return (
     <main className="bg-gray-100 mt-10">
       <div className="base__content pb-12">
-        <BookingDetail />
+        <Suspense fallback={
+          <div className="flex my-20 w-full justify-center items-center space-x-3 p-4 mx-auto rounded-lg text-center">
+            <span className="loader_spiner !border-blue-500 !border-t-blue-200"></span>
+            <span className="text-18">Loading...</span>
+          </div>
+        }>
+          <BookingDetail />
+        </Suspense>
       </div>
     </main>
   );
