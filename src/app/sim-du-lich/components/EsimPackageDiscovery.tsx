aeef64a868e80c469560d4070ca86ea6aace4730
HappyBook Travel <happybook.social@gmail.com>
2026-06-09 10:37:48 +0700

    [080626-87][CLIENT] chưa có data tên sân bay, mặc dù bắt fill ở apply online rồi nhưng vào cms không có và trên frontend cho gợi ý search giống với quốc gia

--- src/app/sim-du-lich/components/EsimPackageDiscovery.tsx
+++ src/app/sim-du-lich/components/EsimPackageDiscovery.tsx
@@ -150,7 +150,6 @@ export default function EsimPackageDiscovery({
             showPricePresetFilters={showPricePresetFilters}
             sortMode={sortMode}
             onSortModeChange={handleSortModeChange}
-            packagesCount={totalPackages ?? packages.length}
           />
         </div>
 
