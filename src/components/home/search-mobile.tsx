675d2bf66a785028402a672461c294645da54942
HappyBook Travel <happybook.social@gmail.com>
2026-06-13 19:34:10 +0700

    [120626-183][CLIENT] FIX UI menu bảo hiểm , thay đổi hình VN bằng zalo OA ở trang vé tàu và trang thuê xe

--- src/components/home/search-mobile.tsx
+++ src/components/home/search-mobile.tsx
@@ -273,24 +273,24 @@ export default function SearchMobile({ airportsData, visaOptionsFilter, comboLoc
         </div>
 
         {/* Quick Links Row */}
-        <div className="grid grid-cols-4 gap-1.5 md:gap-2 mt-0 mb-6 px-3 md:px-1 w-full mx-auto">
+        <div className="grid grid-cols-3 gap-1.5 md:gap-2 mt-0 mb-6 px-3 md:px-1 w-full mx-auto">
           <Link
-            href="/tours"
+            href="/phong-cho-thuong-gia"
             className="rounded-xl text-center h-[80px] block content-center bg-[#00000054] text-white"
           >
             <div className="w-8 h-8 bg-[#175CD3] rounded-full mt-0.5 mx-auto content-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="16px"
-                viewBox="0 -960 960 960"
-                width="16px"
-                fill="#fff"
+              <Image
+                src="/icon/insurance.png"
+                alt="Phòng chờ thương gia"
+                width={16}
+                height={16}
                 className="rounded-full mx-auto"
-              >
-                <path d="M784-120 530-374l56-56 254 254-56 56Zm-546-28q-60-60-89-135t-29-153q0-78 29-152t89-134q60-60 134.5-89.5T525-841q78 0 152.5 29.5T812-722L238-148Zm8-122 54-54q-16-21-30.5-43T243-411q-12-22-21-44t-16-43q-11 59-1.5 118T246-270Zm112-110 222-224q-43-33-86.5-53.5t-81.5-28q-38-7.5-68.5-2.5T296-666q-17 18-22 48.5t2.5 69q7.5 38.5 28 81.5t53.5 87Zm278-280 56-54q-53-32-112-42t-118 2q22 7 44 16t44 20.5q22 11.5 43.5 26T636-660Z" />
-              </svg>
+                style={{ filter: "brightness(0) invert(1)" }}
+              />
             </div>
-            <span className="px-1 mt-0.5 text-[10px] font-semibold block leading-tight">{t("tours")}</span>
+            <span className="px-1 mt-0.5 text-[10px] font-semibold block leading-tight">
+              {t("phong_cho_thuong_gia") || "Phòng chờ"}
+            </span>
           </Link>
           <Link
             href="/du-thuyen"
@@ -309,6 +309,41 @@ export default function SearchMobile({ airportsData, visaOptionsFilter, comboLoc
               </svg>
             </div>
             <span className="px-1 mt-0.5 text-[10px] font-semibold block leading-tight">{t("du_thuyen")}</span>
+          </Link>
+          <Link
+            href="/ve-vui-choi"
+            className="rounded-xl text-center h-[80px] block content-center bg-[#00000054] text-white"
+          >
+            <div className="w-8 h-8 bg-[#175CD3] rounded-full mt-0.5 mx-auto content-center">
+              <Image
+                src="/icon/Ticket.svg"
+                alt="Vé vui chơi"
+                width={16}
+                height={16}
+                className="rounded-full mx-auto"
+              />
+            </div>
+            <span className="px-1 mt-0.5 text-[10px] font-semibold block leading-tight">
+              {t("ve_vui_choi_hoat_dong") || "Vé vui chơi"}
+            </span>
+          </Link>
+          <Link
+            href="/tours"
+            className="rounded-xl text-center h-[80px] block content-center bg-[#00000054] text-white"
+          >
+            <div className="w-8 h-8 bg-[#175CD3] rounded-full mt-0.5 mx-auto content-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="16px"
+                viewBox="0 -960 960 960"
+                width="16px"
+                fill="#fff"
+                className="rounded-full mx-auto"
+              >
+                <path d="M784-120 530-374l56-56 254 254-56 56Zm-546-28q-60-60-89-135t-29-153q0-78 29-152t89-134q60-60 134.5-89.5T525-841q78 0 152.5 29.5T812-722L238-148Zm8-122 54-54q-16-21-30.5-43T243-411q-12-22-21-44t-16-43q-11 59-1.5 118T246-270Zm112-110 222-224q-43-33-86.5-53.5t-81.5-28q-38-7.5-68.5-2.5T296-666q-17 18-22 48.5t2.5 69q7.5 38.5 28 81.5t53.5 87Zm278-280 56-54q-53-32-112-42t-118 2q22 7 44 16t44 20.5q22 11.5 43.5 26T636-660Z" />
+              </svg>
+            </div>
+            <span className="px-1 mt-0.5 text-[10px] font-semibold block leading-tight">{t("tours")}</span>
           </Link>
 
           <Link
